7b0ba4533c84daadcc5798d330de561fc1577eb2
HebersHam <Brendanyos23@gmail.com>
2026-05-10 17:53:43 +1000

    my first commit

---
+++ CALCULATOR MULTIPLICATION.py
@@ -0,0 +1,7 @@
+print ("Hello! Welcome to my calculator")
+number1 = input("Enter first number:")
+number2 = input("Enter second number:")
+number1 = float(number1)
+number2 = float(number2)
+answer = number1*number2
+print("The answer is.... DRUMROLL" + str(answer))
---
+++ JUNK.py
@@ -0,0 +1,7 @@
+print("Hello welcomet to the multiplication tool")
+number1=input("Please enter the first number:")
+number2=input("Please enter the second number:")
+number1=float(number1)
+number2=float(number2)
+result=number1*number2
+print("answer is:" + str(result))
