64be53e755f044390be3c92308697919a3640ea4
HebersHam <Brendanyos23@gmail.com>
2026-05-14 17:56:05 +1000

    Randomised guessing game

--- secret = 7.py
+++ secret = 7.py
@@ -1,10 +1,14 @@
-secret = 7
-print ("Guess the secret number my friend")
-print ("type hint if u need a hint")
-guess = input("what is your guess?").lower()
-if guess ==("hint"):
-    print("the numnber is a single digit")
-elif int(guess)==secret:
-    print("YOU GOT IT RIGHT")
-else:
-    print("WRONG ONE JACKASS")
+import random
+secret = random.randint(1,100)
+print('Hello guess the number and you shall live')
+guesses = 0
+while True:
+    guess = int(input('enter ye guess:'))
+    guesses = guesses + 1
+    if guess < secret:
+        print('too low')
+    elif guess> secret:
+        print('too high')
+    else:
+        print('U win!! ' + str(guesses) + ' guesses used')
+        break
